7dc53e6fcd6fabc0fd4fb5336ecb7466a40f1eaa
vten <viktor.ten@me.com>
2016-08-04 10:14:55 +0600

    Version bump

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(
     name='yqmd',
     packages=['yqmd'],
-    version='0.1.5',
+    version='0.1.6',
     description='Utility lib for working with common period types',
     author='Viktor Ten',
     author_email='ten.viktor@gmail.com',
     url='https://github.com/vibze/yqmd.py',
-    download_url='https://github.com/vibze/yqmd.py/archive/0.1.5.tar.gz',
+    download_url='https://github.com/vibze/yqmd.py/archive/0.1.6.tar.gz',
     keywords=['date']
 )
